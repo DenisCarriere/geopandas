b990a0c6a670c3603b30e96fdea2fff9e9ca3faa
Denis Carriere <carriere.denis@gmail.com>
2019-05-22 21:58:50 -0400

    Push sorted

--- index.py
+++ index.py
@@ -4,16 +4,21 @@ import geopandas
 
 # Read Shapefile
 # <class 'geopandas.geodataframe.GeoDataFrame'>
-geodataframe = geopandas.read_file("shapefile/ne_10m_populated_places_simple.shp")
+gdf = geopandas.read_file("shapefile/ne_10m_populated_places_simple.shp")
 
 # Convert to Pandas DataFrame
 # <class 'pandas.core.frame.DataFrame'>
-dataframe = geodataframe[['latitude', 'longitude', 'name', 'scalerank', 'sov_a3', 'pop_max']]
+df = gdf[['latitude', 'longitude', 'adm0name', 'name', 'scalerank', 'sov_a3', 'pop_max']]
+
+# Filter By Pop Max
+sorted_df = df.groupby(['adm0name']).apply(lambda x: x.sort_values(["pop_max"], ascending = False).head(3))
 
 # Convert Pandas DataFrame back to GeoDataFrame
-geometry = [shapely.geometry.Point(xy) for xy in zip(dataframe['longitude'], df['latitude'])]
+df = sorted_df
+geometry = [shapely.geometry.Point(xy) for xy in zip(df.longitude, df.latitude)]
+df = df.drop(['longitude', 'latitude'], axis=1)
 crs = {'init': 'epsg:4326'}
-geodataframe = geopandas.GeoDataFrame(dataframe, crs=crs, geometry=geometry)
+gdf = geopandas.GeoDataFrame(df, crs=crs, geometry=geometry)
 
 # Save GeoDataFrame to Shapefile
-geodataframe.to_file("output.shp")
+gdf.to_file("output/sorted.shp")
